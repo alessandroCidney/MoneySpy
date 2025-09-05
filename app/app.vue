a89fe35465bc6b913c04f690d2ede4850f39fe3c
Alessandro Cídney <contatoalessandrocidneysa@gmail.com>
2025-09-05 16:48:08 -0300

    feat: Implemented dynamic page title

--- app/app.vue
+++ app/app.vue
@@ -5,3 +5,20 @@
     <layouts-app-message />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+const routes = getRoutes()
+const route = useRoute()
+
+const baseTitle = ref('MoneySpy')
+
+const currentRouteName = computed(() => routes.find(item => item.name === route.name)?.title)
+
+const titlePrefix = computed(() => currentRouteName.value ? `${currentRouteName.value} - ` : '')
+
+const titleStr = computed(() => `${titlePrefix.value}${baseTitle.value}`)
+
+useHead({
+  title: titleStr,
+})
+</script>
